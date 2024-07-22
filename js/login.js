5697194ea3ae266eabdcf727bd7bbcf3cfebd058
SebastianLukito <113852181+SebastianLukito@users.noreply.github.com>
2024-07-22 22:18:06 +0700

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,20 @@
+document.getElementById('loginForm').addEventListener('submit', function(event) {
+    event.preventDefault();
+    
+    const username = document.getElementById('username').value;
+    const password = document.getElementById('password').value;
+
+    fetch('assets/list/data.json')
+        .then(response => response.json())
+        .then(data => {
+            if (data[username] && data[username] === password) {
+                window.location.href = 'index.html';
+            } else {
+                document.getElementById('errorMessage').innerText = 'Salah bosku, anda karyawan mana?';
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching the JSON file:', error);
+            document.getElementById('errorMessage').innerText = 'Error logging in. Please try again later.';
+        });
+});
