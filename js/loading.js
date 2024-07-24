3e3fb82cb23dc37f9b243f400b326e10265731b8
Sebastian <113852181+SebastianLukito@users.noreply.github.com>
2024-07-24 08:59:22 +0700

    Add files via upload

---
+++ js/loading.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    setTimeout(function() {
+        window.location.href = 'loading.html'; // Redirect to the main page after loading
+    }, 3000); // Adjust the timeout duration as needed
+});
