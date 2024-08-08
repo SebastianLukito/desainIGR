ea441621afa13a2129909409b1341b6387c84ca8
Sebastian <113852181+SebastianLukito@users.noreply.github.com>
2024-08-08 15:46:19 +0700

    Add files via upload

--- js/login.js
+++ js/login.js
@@ -1,39 +1,5 @@
 document.getElementById('loginForm').addEventListener('submit', function(event) {
     event.preventDefault();
-    
-    const username = document.getElementById('username').value;
-    const password = document.getElementById('password').value;
-
-    fetch('assets/list/data.json')
-        .then(response => response.json())
-        .then(data => {
-            if (data[username] && data[username] === password) {
-                window.location.href = 'loading.html';
-            } else {
-                document.getElementById('errorMessage').innerText = 'Salah bosku, anda karyawan mana?';
-            }
-        })
-        .catch(error => {
-            console.error('Error fetching the JSON file:', error);
-            document.getElementById('errorMessage').innerText = 'Error logging in. Please try again later.';
-        });
-});
-
-const motorAnimation = document.querySelector('.motor-animation');
-const motorBaruteks = document.querySelector('.motor-baruteks');
-
-motorAnimation.addEventListener('mouseover', () => {
-    motorAnimation.style.display = 'none';
-    motorBaruteks.style.display = 'block';
-});
-
-motorBaruteks.addEventListener('mouseout', () => {
-    motorBaruteks.style.display = 'none';
-    motorAnimation.style.display = 'block';
-});
-
-document.getElementById('loginForm').addEventListener('submit', function(event) {
-    event.preventDefault();
 
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
@@ -60,4 +26,17 @@ function setCookie(name, value, hours) {
     const expires = "expires=" + d.toUTCString();
     document.cookie = name + "=" + value + ";" + expires + ";path=/";
 }
+
+const motorAnimation = document.querySelector('.motor-animation');
+const motorBaruteks = document.querySelector('.motor-baruteks');
+
+motorAnimation.addEventListener('mouseover', () => {
+    motorAnimation.style.display = 'none';
+    motorBaruteks.style.display = 'block';
+});
+
+motorBaruteks.addEventListener('mouseout', () => {
+    motorBaruteks.style.display = 'none';
+    motorAnimation.style.display = 'block';
+});
 
