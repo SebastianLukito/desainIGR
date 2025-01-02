155b7f54af9e3cc6cc98763cc96b169db74df856
Sebastian <113852181+SebastianLukito@users.noreply.github.com>
2025-01-02 15:05:31 +0700

    Add files via upload

--- js/admin.js
+++ js/admin.js
@@ -289,3 +289,16 @@ document.addEventListener('click', function(event) {
         closeAllChildSubButtons();
     }
 });
+
+// Event listener untuk tombol logout
+const logoutBtn = document.getElementById('logoutBtn');
+logoutBtn.addEventListener('click', function() {
+// Hapus cookie 'username'
+document.cookie = "username=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+// Hapus cookie 'loggedIn'
+document.cookie = "loggedIn=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+
+// Arahkan ke halaman login.html
+window.location.href = 'login.html';
+});
