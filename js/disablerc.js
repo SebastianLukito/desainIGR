537b34d5304d0d84a869fd4f60053bdbc4256e67
Sebastian <113852181+SebastianLukito@users.noreply.github.com>
2025-01-22 04:21:15 +0700

    Add files via upload

--- js/disablerc.js
+++ js/disablerc.js
@@ -13,3 +13,18 @@ popupOverlay.addEventListener('click', function (event) {
         popupOverlay.style.display = 'none';
     }
 });
+
+document.addEventListener('keydown', function (e) {
+    // Disable F12
+    if (e.key === 'F12') {
+        e.preventDefault();
+    }
+    // Disable Ctrl+Shift+I
+    if (e.ctrlKey && e.shiftKey && e.key === 'I') {
+        e.preventDefault();
+    }
+    // Disable Ctrl+U
+    if (e.ctrlKey && e.key === 'u') {
+        e.preventDefault();
+    }
+});
